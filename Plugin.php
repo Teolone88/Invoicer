e9fe608cbf4022b10713190f81a3d5f957824e8b
Teo Lo Lo Piparo <36920983+Teolone88@users.noreply.github.com>
2024-06-10 17:31:39 +0200

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -9,19 +9,20 @@ class Plugin extends Base
 {
     public function initialize()
     {
+    	$this->addCspRules();
         // Template Override
         //  - Override name should be camelCase e.g. pluginNameExampleCamelCase
         //$this->template->setTemplateOverride('config/webhook', 'kanboardSupport:config/webhook');
 
         // CSS - Asset Hook - keep filename lowercase
-        //$this->hook->on('template:layout:css', array('template' => 'plugins/KanboardSupport/Assets/css/kanboard-support.css'));
-        //if (!file_exists('plugins/ContentCleaner') || !file_exists('plugins/PluginManager')) {
-        //    $this->hook->on('template:layout:css', array('template' => 'plugins/KanboardSupport/Assets/css/messages.css'));
-        //}
-
+        $this->hook->on('template:layout:css', array('template' => 'plugins/Invoicer/Assets/css/invoicestyle.css'));
         // JS - Asset Hook
         //  - Keep filename lowercase
-        //$this->hook->on('template:layout:js', array('template' => 'plugins/Invoicer/Assets/js/invoicer-support.js'));
+        //$this->hook->on('template:layout:js', array('template' => 'plugins/Invoicer/Assets/js/invoice.js'));
+    	//$this->hook->on('template:layout:js', array('template' => 'plugins/Invoicer/Assets/js/jsPDF_script.js'));
+    	//$this->hook->on('template:layout:js', array('template' => 'plugins/Invoicer/Assets/js/jspdf.umd.min.js'));
+    	//$this->hook->on('template:layout:js', array('template' => 'plugins/Invoicer/Assets/js/html2canvas.min.js'));
+    	//$this->hook->on('template:layout:js', array('template' => 'plugins/Invoicer/Assets/js/jspdf-html2canvas.min.js'));
         // SETTINGS APPLICATION
         $this->template->hook->attach('template:config:application', 'Invoicer:config/invoiceConfig');
         // PROJECT DROPDOWN BUTTON
@@ -29,16 +30,27 @@ class Plugin extends Base
     	//$this->template->hook->attach('template:task:sidebar:information', 'Invoicer:invoice');
         // HELPER
         //$this->helper->register('invoicerHelper', '\Kanboard\Plugin\Invoicer\Helper\invoicerHelper');
+
     }
 
-    public function getClasses()
-    {
-        return array(
-            'Plugin\Invoicer\Model' => array(
-                'invoiceModel',
-            )
-        );
-    }
+	public function addCspRules() 
+	{
+        // get current csp rules
+    	$cspRules = $this->container['cspRules']; 
+    	$root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
+    	// add new domain 
+    	$cspRules['script-src'] =  "'unsafe-inline' 'self' https://fonts.googleapis.com  https://fonts.gstatic.com http://php743.localhost.com https://www.gstatic.com https://cdnjs.cloudflare.com " . $root;
+    	$cspRules['style-src'] =  "'unsafe-inline' 'self' https://fonts.googleapis.com https://fonts.gstatic.com http://php743.localhost.com https://www.gstatic.com https://cdnjs.cloudflare.com " . $root;
+    	$cspRules['font-src'] =  "'unsafe-inline' 'self' https://fonts.googleapis.com https://fonts.gstatic.com http://php743.localhost.com https://www.gstatic.com https://cdnjs.cloudflare.com " . $root;
+    
+    	// update cspRules with the ones we changed
+    	$this->container['cspRules'] = $cspRules;
+	}
+
+    //public function getClasses()
+    //{
+
+    //}
 
     //public function onStartup()
     //{
