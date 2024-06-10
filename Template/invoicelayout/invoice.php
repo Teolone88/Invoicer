054ebf3f711c6585b310a59f33052bc4ee206d4e
Teo Lo Lo Piparo <36920983+Teolone88@users.noreply.github.com>
2024-06-10 17:46:43 +0200

    Update invoice.php

--- Template/invoicelayout/invoice.php
+++ Template/invoicelayout/invoice.php
@@ -1,225 +1,137 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head> 
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <link rel="icon" href='../../Assets/images/begrit-solutions-high-resolution-logo-transparent.png' type="image/x-icon" />
-<style>
-body {
-  @font-face {
-    font-family: 'Droid Sans';
-    font-style: normal;
-    font-weight: 400;
-    src: url('/plugins/Invoicer/font/DroidSansFallback.ttf') format('truetype');
+<div id="wrapper">
+    <div id="pspdfkit-header">
+      <div class="header-columns">
+        <div class="logoty">
+          <img class="logo" src="/client-area/?controller=CustomizerFileController&action=image&plugin=customizer&file_id=1" />
+        </div>
 
-    @font-face {
-      font-family: 'Droid Sans';
-      font-style: normal;
-      font-weight: bold;
-      src: url('/plugins/Invoicer/font/DroidSansFallback.ttf') format('truetype');
+        <div>
+          <p>
+          <?= $this->text->e($values['invFromName']) ?><br>
+<?= $this->text->e($values['invFromAddress1']) ?><br>
+<?= $this->text->e($values['invFromCountry']) ?><br>
+<?= t('VAT ID:  ') ?><?= $this->text->e($values['invFromVAT']) ?>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="page" style="page-break-after: always">
+      <div>
+        <h2><?= t('Invoice #:  ') ?><?= $values['invDate'] . '-' . $this->text->e($project['name']). '-' . $this->text->e($project['id']) ?> <br />
+<?= t('Created:  ') ?><?= $values['invDate'] ?></h2>
+      </div>
 
-      @font-face {
-        font-family: 'Droid Sans';
-        font-style: normal;
-        font-weight: bold;
-        font-style: italic;
-        src: url('/plugins/Invoicer/font/DroidSansFallback.ttf') format('truetype');
+      <div class="intro-table">
+        <div class="intro-form intro-form-item">
+          <p class="intro-table-title">Billed To:</p>
+          <p>
+          <?= $this->text->e($values['invToName']) ?><br>
+<?= $this->text->e($values['invToAddress1']) ?><br>
+<?= $this->text->e($values['invToCountry']) ?><br>
+<?= t('VAT ID:  ') ?><?= $this->text->e($values['invToVAT']) ?>
+          </p>
+        </div>
 
-        * {
-          font-family: Droid Sans, DejaVu Sans, sans-serif;
-        }
+        <div class="intro-form">
+          <div class="intro-form-item-border">
+            <p class="intro-table-title">Payment Due:</p>
+            <p><?= $values['invDue'] ?></p>
+          </div>
 
-        ;
-        color: #777;
-      }
+          <div class="intro-form-item-border">
+            <p class="intro-table-title">Payment Method:</p>
+            <p>Bank Transfer</p>
+          </div>
+        </div>
+      </div>
 
-      body h1 {
-        font-weight: 300;
-        margin-bottom: 0px;
-        padding-bottom: 0px;
-        color: #000;
-      }
+      <div class="table-box">
+        <table cellpadding="0" cellspacing="0" id="empTable">
+          <tbody>
+            <tr class="heading">
+              <td></td>
+              <td>Description</td>
+              <td>Price</td>
+            </tr>
+            <p>
+              <input 
+                type="button" 
+                id="addRow" 
+                value="Add New Item" 
+                onclick="addRow()" />
+            </p>
 
-      body h3 {
-        font-weight: 300;
-        margin-top: 10px;
-        margin-bottom: 20px;
-        font-style: italic;
-        color: #555;
-      }
+            <?php foreach($values['invAdd'] as $task): ?>
+<tr class="item">
+<td><input type="text"  value="" /></td>
+<td><input type="text"  value="<?= t($task) ?>" /></td>
+<td><?= t('100') ?></td>
+</tr>
+<?php endforeach; ?>
+          </tbody>
+        </table>
+      </div>
 
-      body a {
-        color: #06f;
-      }
+      <div class="summary-box">
+        <table cellpadding="0" cellspacing="0">
+          <tbody>
+            <tr class="item">
+              <td></td>
+              <td>Subtotal:</td>
+              <td><input type="text"  value="" /></td>
+            </tr>
 
-      .invoice-box {
-        max-width: 800px;
-        margin: auto;
-        padding: 30px;
-        border: 1px solid #eee;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
-        font-size: 16px;
-        line-height: 24px;
+            <tr class="item">
+              <td></td>
+              <td>Discount:</td>
+              <td><input type="text"  value="" /></td>
+            </tr>
 
-        @font-face {
-          font-family: 'Droid Sans';
-          font-style: normal;
-          font-weight: 400;
-          src: url('/plugins/Invoicer/font/DroidSansFallback.ttf') format('truetype');
+            <tr class="item">
+              <td></td>
+              <td>Subtotal Less Discount:</td>
+              <td><input type="text"  value="" /></td>
+            </tr>
 
-          @font-face {
-            font-family: 'Droid Sans';
-            font-style: normal;
-            font-weight: bold;
-            src: url('/plugins/Invoicer/font/DroidSansFallback.ttf') format('truetype');
+            <tr class="item">
+              <td></td>
+              <td>Total Tax:</td>
+              <td><input type="text"  value="" /></td>
+            </tr>
 
-            @font-face {
-              font-family: Droid Sans';font-style: normal;font-weight: bold; font-style: italic; src: url(' /plugins/Invoicer/font/DroidSansFallback.ttf) format('truetype');
+            <tr class="no-border-item">
+              <td></td>
+              <td>Total Due:</td>
+              <td><input type="text"  value="" /></td>
+            </tr>
 
-              * {
-                font-family: Droid Sans, DejaVu Sans, sans-serif;
-              }
+          </tbody>
+        </table>
+      </div>
+    </div>
 
-              ;
-              color: #555;
-            }
+    <div class="page" style="page-break-after: always">
+      <div class="form">
+        <label for="notes" class="label"> Notes: </label>
+        <input type="text" id="notes" class="border-bottom" value="" />
+      </div>
 
-            .invoice-box table {
-              width: 100%;
-              line-height: inherit;
-              text-align: left;
-              border-collapse: collapse;
-            }
+      <div class="signer">
+        <div class="form signer-item">
+          <label for="date" class="label">Date:</label>
+          <input type="text" id="date" class="border-bottom" value="01/01/2021" />
+        </div>
 
-            .invoice-box table td {
-              padding: 5px;
-              vertical-align: top;
-            }
+        <div class="form signer-item">
+          <label for="signature" class="label">Issued by:</label>
+          <input type="text" id="signature" class="border" value="Sign Here" />
+        </div>
+      </div>
+    </div>
 
-            .invoice-box table tr td:nth-child(2) {
-              text-align: right;
-            }
-
-            .invoice-box table tr.top table td {
-              padding-bottom: 20px;
-            }
-
-            .invoice-box table tr.top table td.title {
-              font-size: 45px;
-              line-height: 45px;
-              color: #333;
-            }
-
-            .invoice-box table tr.information table td {
-              padding-bottom: 40px;
-            }
-
-            .invoice-box table tr.heading td {
-              background: #eee;
-              border-bottom: 1px solid #ddd;
-              font-weight: bold;
-            }
-
-            .invoice-box table tr.details td {
-              padding-bottom: 20px;
-            }
-
-            .invoice-box table tr.item td {
-              border-bottom: 1px solid #eee;
-            }
-
-            .invoice-box table tr.item.last td {
-              border-bottom: none;
-            }
-
-            .invoice-box table tr.total td:nth-child(2) {
-              border-top: 2px solid #eee;
-              font-weight: bold;
-            }
-
-            @media only screen and (max-width: 600px) {
-              .invoice-box table tr.top table td {
-                width: 100%;
-                display: block;
-                text-align: center;
-              }
-
-              .invoice-box table tr.information table td {
-                width: 100%;
-                display: block;
-                text-align: center;
-              }
-            }
-</style>
-	<body>
-		<div class="invoice-box rtl">
-			<table>
-				<tr class="top">
-					<td colspan="2">
-						<table>
-							<tr>
-								<td class="title">
-									<img src='../../images/begrit-solutions-high-resolution-logo-transparent.png' alt="Company logo" style="width: 100%; max-width: 300px" />
-								</td>
-
-								<td>
-									Invoice no.: <?= $values['invDate'] . '-' . $this->text->e($project['name']) . '-' . $this->text->e($project['id']) ?> <br />
-									Created: <?= $values['invDate'] ?>  <br />
-									Due: <?= $values['invDue'] ?>
-								</td>
-							</tr>
-						</table>
-					</td>
-				</tr>
-
-				<tr class="information">
-					<td colspan="2">
-						<table>
-							<tr>
-								<td>
-									<?= $this->text->e($values['invFromName']) ?><br />
-									<?= $this->text->e($values['invFromAddress1']) ?><br />
-									<?= $this->text->e($values['invFromAddress2']) ?><br />
-                                    <?= $this->text->e($values['invFromCityZip']) ?><br />
-                                    <?= $this->text->e($values['invFromCountry']) ?><br />
-                                    <?= $this->text->e($values['invFromVAT']) ?>
-								</td>
-
-								<td>
-									<?= $this->text->e($values['invToName']) ?><br />
-									<?= $this->text->e($values['invToAddress1']) ?><br />
-									<?= $this->text->e($values['invToAddress2']) ?><br />
-                                    <?= $this->text->e($values['invToCityZip']) ?><br />
-                                    <?= $this->text->e($values['invToCountry']) ?><br />
-                                    <?= $this->text->e($values['invToVAT']) ?>
-								</td>
-							</tr>
-						</table>
-					</td>
-				</tr>
-
-				<tr class="heading">
-					<td>Payment Method</td>
-				</tr>
-
-				<tr class="details">
-					<td>Wire Transfer</td>
-				</tr>      
-        		<tr class="heading">
-            		<td>Item</td>
-               		<td>Price</td>	
-        		</tr> 
-                <?php  foreach($values['invAdd'] as $task): ?>
-        			<tr class="item">
-            			<td><?= $task?></td>
-            			<td>100</td>
-        			</tr>               
-                <?php endforeach;?>  
-                
-				<tr class="total">
-					<td></td>
-					<td>Total: $385.00</td>
-				</tr>
-			</table>
-		</div>
-</body>
-</html>
+    <div id="pspdfkit-footer">
+      <div class="footer-columns">
+        <span>Invoice</span>
+      </div>
+    </div>
+  </div>
