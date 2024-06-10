e50f70105deaa2d4c45faa41b44da73375a1d68e
Teo Lo Lo Piparo <36920983+Teolone88@users.noreply.github.com>
2024-06-10 17:47:13 +0200

    Update invoicelayout.php

--- Template/invoicelayout/invoicelayout.php
+++ Template/invoicelayout/invoicelayout.php
@@ -1,14 +1,10 @@
 <section id="main" class="public-project">
-    <a style=" display: inline-block; font-size:12px; text-align: left; width: 100%; word-wrap: break-word;"> 
-	 <?= $this->hook->render('Invoicer:invoicelayout:beforeinvoice') ?>
-    <?= $this->render('Invoicer:invoiceayout/invoice', array(
+<?= $this->render('Invoicer:invoicelayout/invoice', array(
         'project' => $project,
         'values' => $values,
-    	'tasks' => $tasks,
-        'editable' => false,
-        'invoicelayoutprintlayout' => true,
-    )) ?>
-	<?= $this->hook->render('Invoicer:invoicelayout:bottomoflayout') ?>
-    </a>
-    <br>
+        'editable' => false
+)) ?>
 </section>
+</body>
+<button onclick="Convert_HTML_To_PDF();">Convert HTML to PDF</button>
+</html>
